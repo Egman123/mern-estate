86ecc917540ee30d926975d6e31b70ad1da161d0
Exishe <manukyaneghishe@gmail.com>
2024-02-07 16:24:40 +0400

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
    <Router>
+      <Header />
      <Routes>
        <Route path='/' element={<Home />} />
        <Route path='/sign-in' element={<SignIn />} />
